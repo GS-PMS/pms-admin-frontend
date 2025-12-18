4a8d4cdb8193cfc4942a5a00617791e938a56b6e
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-18 22:10:14 +0200

    feat(services): add site service

---
+++ src/app/shared/services/site.service.ts
@@ -0,0 +1,41 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Site } from '../models/site';
+import { PaginatedResponse } from '../models/PaginatedResponse';
+import { Breadcrumb } from '../models/Breadcrumb';
+import { environment } from '../../environments/environment';
+
+const DEFAULT_PAGE = 1;
+const DEFAULT_PAGE_SIZE = 12;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SiteService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = environment.domain;
+
+  getSites(
+    parentId?: string | null,
+    page: number = DEFAULT_PAGE,
+    pageSize: number = DEFAULT_PAGE_SIZE
+  ): Observable<PaginatedResponse<Site>> {
+    let params = new HttpParams()
+      .set('parentId', parentId || '')
+      .set('page', page.toString())
+      .set('pageSize', pageSize.toString());
+
+    return this.http.get<PaginatedResponse<Site>>(this.baseUrl, { params });
+  }
+
+  getSiteById(siteId: string): Observable<Site> {
+    return this.http.get<Site>(`${this.baseUrl}/${siteId}`);
+  }
+
+  getSiteAncestorsIds(siteId: string): Observable<Breadcrumb[]> {
+    return this.http.get<Breadcrumb[]>(`${this.baseUrl}/${siteId}/ancestors`);
+  }
+}
+
+
