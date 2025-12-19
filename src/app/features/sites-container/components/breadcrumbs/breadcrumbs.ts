b42e5413b59af1991ae000ba3a85999fcc1f2a13
Abobakr Osman <mabobakr79@yahoo.com>
2025-12-19 14:11:15 +0200

    refactor: refactor sites container components

--- src/app/features/sites-container/components/breadcrumbs/breadcrumbs.ts
+++ src/app/features/sites-container/components/breadcrumbs/breadcrumbs.ts
@@ -1,5 +1,12 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, of, catchError } from 'rxjs';
+
 import { Breadcrumb } from '../../../../shared/models/Breadcrumb';
+import { SiteService } from '../../../../shared/services/site.service';
+
+const ROOT_BREADCRUMB: Breadcrumb = { id: null, name: 'All sites' };
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,12 +15,45 @@ import { Breadcrumb } from '../../../../shared/models/Breadcrumb';
   styleUrl: './breadcrumbs.scss',
 })
 export class Breadcrumbs {
-  breadcrumbs = input.required<Breadcrumb[]>();
-  currentSiteId = input<string | null>(null);
-  breadcrumbClick = output<Breadcrumb>();
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private siteService = inject(SiteService);
+
+  private routeSiteId = toSignal(
+    this.route.paramMap.pipe(map(params => params.get('siteId'))),
+    { initialValue: null }
+  );
+
+  currentSiteId = computed(() => this.routeSiteId());
+  breadcrumbs = signal<Breadcrumb[]>([ROOT_BREADCRUMB]);
+
+  constructor() {
+    effect(() => {
+      const siteId = this.currentSiteId();
+      if (!siteId) {
+        this.breadcrumbs.set([ROOT_BREADCRUMB]);
+        return;
+      }
+
+      this.siteService
+        .getSiteAncestorsIds(siteId)
+        .pipe(
+          map(ancestorIds => [ROOT_BREADCRUMB, ...ancestorIds]),
+          catchError(() => of([ROOT_BREADCRUMB]))
+        )
+        .subscribe(breadcrumbs => {
+          this.breadcrumbs.set(breadcrumbs);
+        });
+    });
+  }
 
   onBreadcrumbClick(crumb: Breadcrumb): void {
-    this.breadcrumbClick.emit(crumb);
+    const siteId = crumb.id;
+    if (siteId) {
+      this.router.navigate(['/sites', siteId]);
+    } else {
+      this.router.navigate(['/sites']);
+    }
   }
 }
 
